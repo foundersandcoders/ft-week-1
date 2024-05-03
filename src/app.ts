72ab28f687f692c26a8805795d4be11177e296ad
ivan <ivangonzalez@rocketmail.com>
2024-05-03 16:21:57 +0100

    add changes for ssl

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,20 @@
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
+import https from "https";
+import fs from "fs";
+
 import selectRoutes from "./api/selectRoutes";
 import patchRoutes from "./api/patchRoutes";
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 443;
 
 const app = express();
+
+const options = {
+  key: fs.readFileSync("../etc/ssl/certs/key.pem"),
+  cert: fs.readFileSync("../etc/ssl/certs/cert.pem"),
+};
 
 //Middleware
 app.use(cors());
@@ -14,4 +22,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/api/questions/", selectRoutes);
 app.use("/api/questions/", patchRoutes);
 
-app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
+const httpServer = https.createServer(options, app);
+
+httpServer.listen(PORT, () =>
+  console.log(`Listening on http://localhost:${PORT}`),
+);
